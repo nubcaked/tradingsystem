ccd547c4850a084ff452629aebc423f0553f7eab
nubcaked <37624241+nubcaked@users.noreply.github.com>
2020-02-15 16:43:18 +0800

    first commit

---
+++ src/main/java/com/liangwei/tradingsystem/TradingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.liangwei.tradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingsystemApplication.class, args);
+	}
+
+}
