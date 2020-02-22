bd379da89209c65a8d09bcaffa89f0f56bf7809a
nubcaked <37624241+nubcaked@users.noreply.github.com>
2020-02-22 14:39:27 +0800

    updated positions.csv | added new method in security repo | changes to security service incomplete

--- src/main/java/com/liangwei/tradingsystem/repository/SecurityRepository.java
+++ src/main/java/com/liangwei/tradingsystem/repository/SecurityRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.scheduling.annotation.Async;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SecurityRepository extends JpaRepository<Security, Long> {
@@ -14,4 +15,6 @@ public interface SecurityRepository extends JpaRepository<Security, Long> {
     <S extends Security> S save(S entity);
 
     Optional<Security> findByTicker(String ticker);
+
+    List<Security> findByParentTicker(String parentTicker);
 }
--- src/main/java/com/liangwei/tradingsystem/service/SecurityService.java
+++ src/main/java/com/liangwei/tradingsystem/service/SecurityService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -54,30 +55,59 @@ public class SecurityService {
         return result;
     }
 
+    //TODO: make this method call updateStockPrice() and updateOptionPrice()
     @Async
     public void moveStockPrice(Security security) {
         while (dataProviderFlag.isRunFlag()) {
-            double price = security.getPrice();
-            int deltaTime = ThreadLocalRandom.current().nextInt(500, 1501);
-            double deltaTimeSeconds = deltaTime / 1000.0;
-            double expectedReturn = security.getExpectedReturn();
-            double standardDeviation = security.getStandardDeviation();
-            double standardNormalDistribution = new NormalDistribution().sample();
-
-            double deltaPrice = price * (expectedReturn * deltaTimeSeconds / 7257600 + standardDeviation * standardNormalDistribution * Math.sqrt(deltaTimeSeconds / 7257600));
-            double newPrice = price + deltaPrice;
-            try {
-                Thread.sleep(deltaTime);
-                security.setPrice(newPrice);
-                securityRepository.save(security);
-                eventBus.post(security);
-//                if (security.getTicker().equals("GOOG")) {
-//                    System.out.println(security.getPrice());
-//                }
-            } catch (InterruptedException e) {}
-//            System.out.println("hihi hoho " + security.getTicker());
-//            eventBus.post("hihi hoho " + security.getTicker());
+            updateStockPrice(security);
+            updateOptionPrice(security);
         }
+    }
+
+    public void updateStockPrice(Security stock) {
+        double price = stock.getPrice();
+        int deltaTime = ThreadLocalRandom.current().nextInt(500, 1501);
+        double deltaTimeSeconds = deltaTime / 1000.0;
+        double expectedReturn = stock.getExpectedReturn();
+        double standardDeviation = stock.getStandardDeviation();
+        double standardNormalDistribution = new NormalDistribution().sample();
+
+        double deltaPrice = price * (expectedReturn * deltaTimeSeconds / 7257600 + standardDeviation * standardNormalDistribution * Math.sqrt(deltaTimeSeconds / 7257600));
+        double newPrice = price + deltaPrice;
+        try {
+            Thread.sleep(deltaTime);
+            stock.setPrice(newPrice);
+            securityRepository.save(stock);
+//                eventBus.post(security);
+        } catch (InterruptedException e) {}
+    }
+
+    public void updateOptionPrice(Security stock) {
+        try {
+//            Security stock = securityRepository.findByTicker(option.getParentTicker()).get();
+            String ticker = stock.getTicker();
+            Security option = securityRepository.findByTicker(ticker).get(); //TODO: findByParentTicker() instead
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+            double stockPrice = stock.getPrice();
+            double standardDeviation = stock.getStandardDeviation();
+            double strikePrice = option.getStrikePrice();
+            double interestRate = 0.02;
+            long timeToMaturity = (simpleDateFormat.parse(option.getMaturityDate()).getTime() - new GregorianCalendar().getTimeInMillis()) / 31536000000L; // 1000 * 60 * 60 * 24 * 365 = 31536000000
+            double d1 = (Math.log(stockPrice / strikePrice) + (interestRate + Math.pow(standardDeviation, 2) / 2) * timeToMaturity) / (standardDeviation * Math.sqrt(timeToMaturity));
+            double d2 = d1 - (standardDeviation * Math.sqrt(timeToMaturity));
+
+            double callOptionPrice = (stockPrice * new NormalDistribution().cumulativeProbability(d1)) - (strikePrice * Math.exp(-interestRate * timeToMaturity) * new NormalDistribution().cumulativeProbability(d2));
+            double putOptionPrice = (strikePrice * Math.exp(-interestRate * timeToMaturity) * new NormalDistribution().cumulativeProbability(-d2)) - (stockPrice * new NormalDistribution().cumulativeProbability(-d1));
+
+            //TODO: save the option price to db
+//            if (option.getType().equals("call")) {
+//                return callOptionPrice;
+//            } else if (option.getType().equals("put")) {
+//                return putOptionPrice;
+//            }
+        } catch (Exception e) {}
     }
 
 }
